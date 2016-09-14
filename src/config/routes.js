d54a14d2226c1847eafc3211e6a55d87f33bec1c
aridoza <aridoza85@gmail.com>
2016-09-14 19:23:40 -0400

    render through routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-import App from '../components/App';
+import App from '../containers/App';
 
 const routes = (
   <Router history={hashHistory}>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ReactDOM, render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
-import App from './components/App';
+import App from './containers/App';
 import routes from './config/routes';
 
 render(
