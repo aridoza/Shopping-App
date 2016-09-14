5ba145e739bceb0625af49bd1b344510560fd2c2
aridoza <aridoza85@gmail.com>
2016-09-14 19:12:33 -0400

    render through routes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import { ReactDOM, render } from 'react-dom';
 import App from './components/App';
 import routes from './config/routes';
 
-render(<App name='World'/>, document.getElementById('shopping-app'));
+ReactDOM.render(
+  routes,
+  document.getElementById('shopping-app')
+);
