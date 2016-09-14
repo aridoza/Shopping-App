7710cde023eb71f8bc2ed24c1c74ee14d23a90c2
aridoza <aridoza85@gmail.com>
2016-09-14 19:33:57 -0400

    adding navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 import '../assets/base.scss';
 import React, { Component } from 'react';
+import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 const App = React.createClass({
   render() {
+
+    let navStyle = {
+      backgroundColor: 'white',
+      borderTop: '2px solid #B8860B',
+      color: '#B8860B',
+      textDecoration: 'none',
+      zIndex: '1'
+    }
+
+    let pageStyle = {
+      display: 'flex',
+      position: 'relative',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      height: '100%',
+      width: '100%',
+      background: 'white'
+    }
+
     return(
-      <div>
-        <h1>Hello, {this.props.name}!</h1>
+      <div className="app-container" style={pageStyle}>
+        <ul style={navStyle}>
+          <Nav bsStyle="pills">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Products</li>
+          </Nav>
+        </ul>
       </div>
     )
   }
