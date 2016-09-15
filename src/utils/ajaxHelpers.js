49d8ab26a1298aa3ac3eaf900b5e69330e52247c
aridoza <aridoza85@gmail.com>
2016-09-15 12:28:42 -0400

    working in mlab db to axioshelpers

--- src/utils/ajaxHelpers.js
+++ src/utils/ajaxHelpers.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const helpers = {
   getProducts: function(){
-    return axios.get('mongodb://heroku_pcbjw1kg:40n682pbj0aua5d5n5hardjtut@ds033116.mlab.com:33116/heroku_pcbjw1kg');
+    return axios.get('mongodb://heroku_pcbjw1kg:40n682pbj0aua5d5n5hardjtut@ds033116.mlab.com:33116/heroku_pcbjw1kg/popsicles');
   }
 }
 
