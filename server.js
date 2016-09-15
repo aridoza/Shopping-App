edb71b040560c376b0261e48b0260523d9e3a264
aridoza <aridoza85@gmail.com>
2016-09-15 13:25:28 -0400

    testing mlab

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 8080
+const MONGOLAB_URI= 'mongodb://heroku_pcbjw1kg:40n682pbj0aua5d5n5hardjtut@ds033116.mlab.com:33116/heroku_pcbjw1kg';
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//define Mongoose Schema
+Schema = new mongoose.Schema({
+  id       : String,
+  title    : String,
+  completed: Boolean
+}),
+
+Todo = mongoose.model('Todo', Schema);
+
+
+
+mongoose.connect(process.env.MONGOLAB_URI, function (err){
+  if (err) {
+    console.log("mongooseconnect error:",err);
+  } else {
+    console.log("Mongo connected");
+  }
+});
+
+app.get('/', function (req, res) {
+  res.json(200, {msg: 'OK'});
+})
+
+app.get('/popsicles', function (req, res){
+  popsicles.find( function (err, result){
+    res.json(200, result);
+  });
+})
+
+app.use(express.static(__dirname + '/'))
+app.listen(process.env.PORT || 5000);
+
+
 
 // using webpack-dev-server and middleware in development environment
 if(process.env.NODE_ENV !== 'production') {
@@ -17,14 +57,14 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
-});
-
-app.listen(PORT, function(err) {
-  if (err) {
-    console.log("Error:", err);
-  } else {
-    console.info("Listening on port " + PORT + ".");
-  }
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/dist/index.html')
+// });
+//
+// app.listen(PORT, function(err) {
+//   if (err) {
+//     console.log("Error:", err);
+//   } else {
+//     console.info("Listening on port " + PORT + ".");
+//   }
+// });
