2069ec232e4d0635eeb2e984a6843a75e7093e2c
aridoza <aridoza85@gmail.com>
2016-09-14 21:16:48 -0400

    adding database

--- src/private/app.js
+++ src/private/app.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoClient = mongodb.MongoClient;
-
+const mongoUrl = 'mongodb://localhost:27017/ProductsAlpha';
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
