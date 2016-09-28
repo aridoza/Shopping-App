bfd26d1c9957e03df5eded42f110a52987af3d58
aridoza <aridoza85@gmail.com>
2016-09-28 10:10:35 -0400

    changed server and axios to MONGOLAB_BLUE_URI

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + "/public"));
 
 var db;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGOLAB_BLUE_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
